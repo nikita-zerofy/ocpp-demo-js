144e8941056db2e10c60a4652047207b022ff9e3
Nikita Baranov <nikita@zerofy.net>
2025-02-14 15:35:26 +0200

    update logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,17 @@
 import pino, { Logger } from 'pino';
-import { createGcpLoggingPinoConfig } from '@google-cloud/pino-logging-gcp-config';
 import config from './config';
 
-const isProduction = config.nodeEnv === 'production';
 const logLevel = config.logLevel || 'trace';
 
-let logger: Logger;
-
-if (isProduction) {
-  logger = pino({ ...createGcpLoggingPinoConfig(), level: logLevel });
-} else {
-  logger = pino({
-    level: logLevel,
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname',
-      },
-    },
-  });
-}
+const logger: Logger = pino({
+  level: logLevel,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname'
+    }
+  }
+});
 
 export default logger;
