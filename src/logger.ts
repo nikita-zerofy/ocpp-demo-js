f06af1c905bfe118dcebb733212a3a25ba6a1277
Nikita Baranov <nikita@zerofy.net>
2025-02-14 15:33:00 +0200

    fix logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,21 +1,25 @@
-import pino from 'pino';
-import {createGcpLoggingPinoConfig} from '@google-cloud/pino-logging-gcp-config';
+import pino, { Logger } from 'pino';
+import { createGcpLoggingPinoConfig } from '@google-cloud/pino-logging-gcp-config';
 import config from './config';
 
 const isProduction = config.nodeEnv === 'production';
 const logLevel = config.logLevel || 'trace';
 
-const logger = isProduction
-  ? pino({...createGcpLoggingPinoConfig(), level: logLevel})
-  : pino({
-      level: logLevel,
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          translateTime: 'SYS:standard',
-          ignore: 'pid,hostname',
-        },
+let logger: Logger;
+
+if (isProduction) {
+  logger = pino({ ...createGcpLoggingPinoConfig(), level: logLevel });
+} else {
+  logger = pino({
+    level: logLevel,
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'SYS:standard',
+        ignore: 'pid,hostname',
       },
-    });
+    },
+  });
+}
 
 export default logger;
